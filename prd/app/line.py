725f4ebfd1134f54ff1b39e1c6132b1a0741d031
d_yokota <d_yokota@67-16546.local>
2023-03-25 15:01:07 +0900

    update: DB接続 && レスポンス構築

--- prd/app/line.py
+++ prd/app/line.py
@@ -2,6 +2,8 @@ import os
 from dotenv import load_dotenv
 from waitress import serve
 from flask import Flask, request, abort
+import sqlite3
+import pandas as pd
 
 from linebot import (
     LineBotApi, WebhookHandler
@@ -13,16 +15,40 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
 
+def req_input(input_str : str)-> list:
+    
+    conn = sqlite3.connect('../db/zaim.db')
+    df = pd.read_sql(get_guery(shohin=input_str, filename="shohin.sql"), conn)
+    result = df.to_dict(orient='records')
+    return result
+
+
+def get_guery(shohin: str, filename: str)->str:
+    """
+    SQLファイルに変数を挿入してクエリを作成する
+
+    Args:
+        num (int): 年齢
+        category (str): 好きな果物
+        filename (str): SQLファイルの名前
+
+    Returns:
+        str: SQLクエリ
+    """
+    with open(os.path.join("./sql/", filename), "r") as f:
+        return f.read().format(shohin=shohin)
+
+
 app = Flask(__name__)
 
 #環境変数として読み出し
 load_dotenv()
 LINE_ACCESS_TOKEN = os.getenv('LINE_ACCESS_TOKEN')
-LINE_USER_ID = os.getenv('LINE_USER_ID')
+LINE_CHANNEL = os.getenv('LINE_CHANNEL')
 
 
 line_bot_api = LineBotApi(LINE_ACCESS_TOKEN)
-handler = WebhookHandler(LINE_USER_ID)
+handler = WebhookHandler(LINE_CHANNEL)
 
 @app.route("/")
 def hello_world():
@@ -50,9 +76,20 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
 
+    #テキストの生成
+    text = ""
+    data =req_input(event.message.text)
+    if(data[0] != ''):
+        for item in data:
+            text += f"{item['日付']} に {item['商品名']} を購入しています。\n"
+            text += f"金額: {item['金額']}\n"
+            text += f"お店: {item['お店']}\n\n"
+    else:
+        text = '購入履歴がありませんでした。'
+
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text=text))
 
 
 if __name__ == "__main__":
--- prd/app/send.py
+++ prd/app/send.py
@@ -8,13 +8,11 @@ from linebot import (
 
 import json
 import os
+import sqlite3
+import pandas as pd
 from linebot import (LineBotApi, WebhookHandler)
 from linebot.models import (MessageEvent, TextMessage, TextSendMessage)
 from linebot.exceptions import (LineBotApiError, InvalidSignatureError)
-
-# app = Flask(__name__)
-
-
 
 
 def lambda_handler():
@@ -22,13 +20,53 @@ def lambda_handler():
     #環境変数として読み出し
     load_dotenv()
     LINE_ACCESS_TOKEN = os.getenv('LINE_ACCESS_TOKEN')
+    LINE_USER_ID = os.getenv('LINE_USER_ID')
+
+
+    #テキストの生成
+    text = ""
+    data =req_input('肉だんご')
+    if(data[0] != ''):
+        for item in data:
+            text += f"{item['日付']} に {item['商品名']} を購入しています。\n"
+            text += f"金額: {item['金額']}\n"
+            text += f"お店: {item['お店']}\n\n"
+    else:
+        text = '購入履歴がありませんでした。'
+
+
+    #message API
     line_bot_api = LineBotApi(channel_access_token=LINE_ACCESS_TOKEN )
-    line_bot_api.push_message("U370c2a0bea92c2a8b36ac821740510bd" , TextSendMessage(text='Hello World!'))
+    line_bot_api.push_message(LINE_USER_ID , TextSendMessage(text=text))
 
     return {
         'statusCode': 200,
         'body': json.dumps('ok', ensure_ascii=False)
     }
 
+def req_input(input_str : str)-> list:
+    
+    conn = sqlite3.connect('../db/zaim.db')
+    df = pd.read_sql(get_guery(shohin=input_str, filename="shohin.sql"), conn)
+    result = df.to_dict(orient='records')
+    return result
+
+
+def get_guery(shohin: str, filename: str)->str:
+    """
+    SQLファイルに変数を挿入してクエリを作成する
+
+    Args:
+        num (int): 年齢
+        category (str): 好きな果物
+        filename (str): SQLファイルの名前
+
+    Returns:
+        str: SQLクエリ
+    """
+    with open(os.path.join("./sql/", filename), "r") as f:
+        return f.read().format(shohin=shohin)
+
+    
 if __name__ == "__main__":
     print(lambda_handler())
